85c4f13f085cb6e49d5b7115294e163d1f6f563c
adminctic <piero.torres@uni.edu.pe>
2016-12-19 14:54:51 -0500

    Filter Main funciones

--- MTA_filter_main.py
+++ MTA_filter_main.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext, Row
 from datetime import datetime
@@ -29,7 +24,7 @@ def cleanEl(x):
         return r
 
 
-# In[3]:
+
 
 def forTim(x):
         fecha=datetime.strptime(x[2], '%Y-%m-%d %H:%M:%S')
@@ -42,44 +37,63 @@ def toHourBucket(x):
         x[4]= timeStamp//horaEnSeg
         return x
 
-
-# In[4]:
-
-rdd = sc.textFile('file:///home/cloudera/Downloads/MTA-Bus-Time_.2014-08-01.txt')
-
-
-# In[6]:
-
-#limpia ingresos repetidos
-cleaned=rdd.distinct()
+def mainFilter(rdd):
+	#limpia ingresos repetidos
+	cleaned=rdd.distinct()
 
 
-#descarta lineas corruptas que son aquellas que no cumplen las 26 comillas
-discardCorrupted= cleaned.filter(lambda x: countTab(x)==10)
+	#descarta lineas corruptas que son aquellas que no cumplen las 26 comillas
+	discardCorrupted= cleaned.filter(lambda x: countTab(x)==10)
 
-#va a mapear cada ingreso y lo  partira en elementos a traves de la tabulacin
-filteredT= discardCorrupted.map(lambda x:x.split('\t'))
+	#va a mapear cada ingreso y lo  partira en elementos a traves de la tabulacin
+	filteredT= discardCorrupted.map(lambda x:x.split('\t'))
 
+	#descarta las lineas que son headers
+	discardHeaders=filteredT.filter(lambda x : x[0]!="latitude" )
 
-#descarta las lineas que son headers
-discardHeaders=filteredT.filter(lambda x : x[0]!="latitude" )
+	#formatea los tiempos a segundos en UNX
+	formatTime= discardHeaders.map(lambda x: forTim(x))
 
-#formatea los tiempos a segundos en UNX
-formatTime= discardHeaders.map(lambda x: forTim(x))
+	#Se genera una tupla para clasificacion con los valores timestamp, busID,orientation, nextStop, Route
+	classTuple= formatTime.map(lambda x: str(x[2])+","+x[3]+","+x[5]+","+x[10]+","+x[7])
 
-#solo mostrara los ingresos para una ruta
-#getResultsFromOneRoute= formatTime.filter(lambda x: x[7]=='MTA NYCT_B63' )
-
-#solo mostrara los ingresos para un bus
-#getResultsFromOneBus= formatTime.filter(lambda x: x[3]=='469')
+	return classTuple
 
 
-#Se genera una tupla para clasificacion con los valores timestamp, busID,orientation, nextStop, Route
-classTuple= formatTime.map(lambda x: str(x[2])+","+x[3]+","+x[5]+","+x[10]+","+x[7])
+##########################################################################
+#conf = SparkConf().setMaster("spark://King:7077").setAppName("MTAMain")
+
+#sc = SparkContext(conf = conf)
+
+#rdd = sc.textFile('hdfs://King:9000/user/bdata/mta_data/MTA-Bus-Time_.2014-08-01.txt')
+
+
+###########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #collectedTuple=classTuple.collect()
 
-classTuple.saveAsTextFile('hdfs:/user/cloudera/wordcount/classification_buses.csv')
+#classTuple.saveAsTextFile('hdfs:/user/cloudera/wordcount/classification_buses.csv')
 #classTuple.saveAsTextFile('file:///home/cloudera/Downloads/classification_buses.csv')
 #collectedTuple.saveAsTextFile('file:///home/cloudera/Downloads/classification_buses.csv')
 
